a72c58da95ed57447878f1715395d69c18b3beaf
briankim <brianhkim13@gmail.com>
2024-01-20 19:34:56 -0600

    frustrated but will not give up this is the beginning

--- app.rb
+++ app.rb
@@ -1,17 +1,22 @@
 require 'sinatra'
-require 'sinatra/activerecord'
-require 'rest-client'
+require 'net/http'
 require 'json'
-require 'better_errors'
-require 'binding_of_caller'
-require 'table_print'
-
-# Set up API base URL
-@url = 'https://ped.uspto.gov/api/'
-@response = HTTP.get(@url)
-@data = JSON.parse(@response)
 
 get("/") do
-
-  erb(:home)
+  author = params['author']
+  url = URI("https://poetrydb.org/author/#{URI.encode_www_form_component(author)}/title")
+  response = Net::HTTP.get_response(url)
+  
+  if response.body.empty? || response.body == '<h1>Not Found</h1>'
+    @result = "No response received from API or author not found"
+  else
+    begin
+      data = JSON.parse(response.body)
+      @result = data
+    rescue JSON::ParserError
+      @result = "Response is not valid JSON"
+    end
+  end
+  
+  erb :home
 end
